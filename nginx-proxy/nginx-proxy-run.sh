afa8f2134235251e1ba1955c989139c9365dbbff
Vincent Flesouras <x.algorithm@gmail.com>
2016-08-23 11:20:13 -0700

    Add interaction with conf

--- nginx-proxy/nginx-proxy-run.sh
+++ nginx-proxy/nginx-proxy-run.sh
@@ -1,4 +1,5 @@
 #docker pull jwilder/nginx-proxy
+docker exec -d nginx-proxy cat /etc/nginx/conf.d/default.conf > ./default.conf
 docker stop nginx-proxy
 docker rm nginx-proxy
-docker run --name="nginx-proxy" -d -p 80:80 -v /var/run/docker.sock:/tmp/docker.sock:ro jwilder/nginx-proxy
+docker run --name="nginx-proxy" -d -p 80:80 -v default.conf:/etc/nginx/conf.d/default.conf:rw  -v /var/run/docker.sock:/tmp/docker.sock:ro jwilder/nginx-proxy 
