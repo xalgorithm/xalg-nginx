358acdf4ccb02c47cbcdc40e2db32f30856cba10
Vincent Flesouras <x.algorithm@gmail.com>
2016-08-25 11:34:24 -0700

    fix mismatch on folder name

--- bin/nginx-ashley-run.sh
+++ bin/nginx-ashley-run.sh
@@ -4,7 +4,7 @@ cd /opt/docker-files/ansible-docker-nginx/docker
 docker build -t xalg/nginx /opt/docker-files/ansible-docker-nginx/docker/
 docker run -d \
     --name ashley \
-    -e WEB_DOCUMENT_ROOT=/app/amcf \
+    -e WEB_DOCUMENT_ROOT=/app/ashley \
     -e WEB_ALIAS_DOMAIN="ashleythenewbie.com,*.ashleythenewbie.com" \
     -e VIRTUAL_HOST="ashleythenewbie.com,*.ashleythenewbie.com" \
     --restart=always \
